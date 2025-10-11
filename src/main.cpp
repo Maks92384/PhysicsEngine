88df2fc11d891a40990e301abfdf4db7b670eb46
Maks <166073630+Maks92384@users.noreply.github.com>
2025-10-11 17:06:33 +0200

    MomentOfInertiaDemo commit

--- src/main.cpp
+++ src/main.cpp
@@ -26,64 +26,21 @@ int main() {
     sf::Clock clock;
     unsigned int deltaTime = 0; // Time between frames in microseconds
 
-    //PhysicsObject& object = PhysicsEngine::createObject({2, 2 / (float) sqrt(2), 0});
     PhysicsObject& object = PhysicsEngine::createObject({0, 0, 0});
 
-    //object.setOrientation({0, 0, -M_PI / 2});
-    //object.setPosition({2, 2 * cos(object.getOrientation().z), 0});
-
-    object.setAngularVelocity({-20, 0, 0});
-    //object.setOrientation(Quaternion(1, 0, 1, 0));
-
-    //Constraints::addPlaneConstraint({0, 0, 0}, 1);
+    //object.setAngularVelocity({-20, 0, 0});
 
     while (window.isOpen()) {
         clock.restart();
 
         manageEvents(window);
+
         // Moment of inertia DEMO
 
-        object.applyForce({-2, 0, 0}, {0, -10, 0});
-        object.applyForce({2, 0, 0}, {0, 10, 0});
+        //object.applyForce({-2, 0, 0}, {0, -10, 0});
+        //object.applyForce({2, 0, 0}, {0, 10, 0});
 
-
-        /*  ROCKET SIMULATOR
-
-        object.applyForce({0, 0, 0}, {0, -1, 0});
-
-        sf::Vector3f rocketEngineDirection = {0, 0, 0};
-        sf::Vector3f rocketEngineForce = {0, 0, 0};
-        float rocketEnginePower = 1.5;
-        float rocketEngineGimbalAngle = M_PI / 12;
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
-            rocketEngineDirection += {1, 0, 0};
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-            rocketEngineDirection += {-1, 0, 0};
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
-            rocketEngineDirection += {0, 0, 1};
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-            rocketEngineDirection += {0, 0, -1};
-        if (rocketEngineDirection.length() != 0)
-            rocketEngineDirection = rocketEngineDirection.normalized() * sin(rocketEngineGimbalAngle) * rocketEnginePower;
-        rocketEngineForce = {rocketEngineDirection.x, cos(rocketEngineGimbalAngle) * rocketEnginePower, rocketEngineDirection.z};
-
-        object.applyForce({0, -2, 0}, Engine3D::rotateWithEulerVector(rocketEngineForce, object.getOrientation()));
-        */
-
-
-        /*
-        0.388229 1.44889 0
-        cout<<"-----------------------------------"<<endl;
-        -0.388229 1.39952 0.375
-        cout<<rocketEngine.x<<" "<<rocketEngine.y<<" "<<rocketEngine.z<<endl;
-        cout<<object.getVelocity().x<<" "<<object.getVelocity().y<<" "<<object.getVelocity().z<<endl;
-        cout<<object.getOrientation().x<<endl;
-        */
-
-        //if (object.getPosition().y > -1)
-        //if (object.getOrientation().toEulerVector().z < M_PI / 2)
-            PhysicsEngine::update(deltaTime);
+        PhysicsEngine::update(deltaTime);
         PhysicsEngine::show();
 
         Camera::updateCamera(window);
